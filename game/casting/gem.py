2e2203bb51f60b3e05e2a2f13c403e5d4cc66c1b
melindahslaughter <mslaughter1@byui.edu>
2022-11-01 22:40:44 -0600

    added comments to gem.py

--- game/casting/gem.py
+++ game/casting/gem.py
@@ -1,7 +1,7 @@
 from game.casting.artifact import Artifact
 
-class Gem(Artifact):
+class Gem(Artifact): #gem is a child class of artifact
   
     def __init__(self):    
-        super().__init__()
-        self._text = "*"
+        super().__init__()  #the shape of the artifact gem is created.
+        self._text = "*"     # one point is added when the robot meets with a gem. 
